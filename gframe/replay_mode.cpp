44128da12e3926231950911dede387aa0972a314
Antonio Curavalea <one.kyonblack@gmail.com>
2025-11-16 23:56:56 +0200

    feat: wait briefly before exiting replay mode

--- gframe/replay_mode.cpp
+++ gframe/replay_mode.cpp
@@ -167,8 +167,9 @@ void ReplayMode::EndDuel() {
 		mainGame->stMessage->setText(gDataManager->GetSysString(1501).data());
 		if(mainGame->wCardSelect->isVisible())
 			mainGame->HideElement(mainGame->wCardSelect);
-		mainGame->PopupElement(mainGame->wMessage);
-		mainGame->actionSignal.Wait(lock);
+		// mainGame->PopupElement(mainGame->wMessage);
+		// mainGame->actionSignal.Wait(lock);
+		mainGame->WaitFrameSignal(60, lock);
 		mainGame->dInfo.isInDuel = false;
 		mainGame->dInfo.isStarted = false;
 		mainGame->dInfo.isReplay = false;
